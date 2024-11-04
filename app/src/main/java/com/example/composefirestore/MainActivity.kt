41de55316e0b9839b8ae1873a16eecf735882b2e
林庚賢 <garylin9211@gmail.com>
2024-11-04 15:42:37 +0800

    v2_姓名輸入

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -1,31 +1,24 @@
 package com.example.composefirestore
 
+import android.R.attr.label
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composefirestore.ui.theme.ComposefirestoreTheme
-
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.composefirestore.ui.theme.ComposefirestoreTheme
 
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.material3.Button
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,8 +40,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+private infix fun Modifier.label(any: Any): Any {
+    TODO("Not yet implemented")
+}
 
+@Composable
 fun Birth(m: Modifier){
+    var userName by remember { mutableStateOf("林庚賢")}
+
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            modifier = m,
+            label = {Text("姓名")},
+            placeholder = {Text("請輸入你的姓名")}
+
+
+
+
+        )
+
+        Text("您輸入的姓名是：$userName")
+    }
 
 }
